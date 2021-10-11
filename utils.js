ee2251f5c0dd672c42cb8e225883c475761e338f
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-11 10:28:24 +0530

    Add headers

--- utils.js
+++ utils.js
@@ -22,7 +22,12 @@ const selectors = {
     }
 }
 const getProductDetails = async(url, merchant) => {
-    const {data} = await axios.get(url, {headers: { "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36", }});
+    const {data} = await axios.get(url, {
+        headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+          },
+    });
     try{
         const $ = cheerio.load(data);
         const selector = selectors[merchant];
