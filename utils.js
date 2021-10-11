80db136afcfba3065b78a4feaf23138446ae2f94
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-11 21:04:23 +0530

    switch to worker

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const {WORKER_URL} = require('./config');
 
 const urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
@@ -16,14 +17,14 @@ const selectors = {
         image: '#landingImage'
     },
     flipkart: {
-        price: '#container > div > div._2c7YLP.UtUXW0._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div._1YokD2._3Mn1Gg.col-8-12 > div:nth-child(2) > div > div.dyC4hf > div.CEmiEU > div > div._30jeq3._16Jk6d',
+        price: '._30jeq3._16Jk6d',
         title: '.B_NuCI',
-        image: '#container > div > div._2c7YLP.UtUXW0._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div._1YokD2._3Mn1Gg.col-5-12._78xt5Y > div:nth-child(1) > div > div._3li7GG > div._1BweB8 > div._3kidJX > div.CXW8mj._3nMexc > img'
+        image: '.CXW8mj._3nMexc > img'
     }
 }
 const getProductDetails = async(url, merchant) => {
     try{
-        const res = await axios.get(url, {
+        const res = await axios.get(`${WORKER_URL}/?url=${encodeURIComponent(url)}`, {
             headers: {
                 "User-Agent":
                   "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
