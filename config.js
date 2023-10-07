f4d2f995be547cd69390cdccc5a6f091eb914d7e
siddiquiaffan <siddiquiaffan201@gmail.com>
2023-10-07 22:24:53 +0530

    Add HTTPS Proxy URL

--- config.js
+++ config.js
@@ -11,11 +11,25 @@ if(!process.env.DB_URL) {
     process.exit(1);
 }
 
-const ADMINS = process.env.ADMINS || ''
-const BOT_TOKEN = process.env.BOT_TOKEN || ''
-const DB_URL = process.env.DB_URL || ''
-const WORKER_URL = process.env.WORKER_URL || ''
-const API_KEY = process.env.API_KEY || '' // Generate any API Key and pass it when accessing the API.
+/** List of amdins (tg IDs) separated by space */
+const ADMINS = process.env.ADMINS ?? ''
+
+/** Telegram bot token */
+const BOT_TOKEN = process.env.BOT_TOKEN ?? ''
+
+/** HTTPS Proxy URL */
+const HTTPS_PROXY = process.env.PROXY ?? ''
+
+/** MongoDB URL */
+const DB_URL = process.env.DB_URL ?? ''
+
+/** Cloudflare Worker URL */
+const WORKER_URL = process.env.WORKER_URL ?? ''
+
+/** API Key - A random secure key to access api */
+const API_KEY = process.env.API_KEY ?? '' // Generate any API Key and pass it when accessing the API.
+
+/** Maximum number of products can be added by a user at a time. */
 const LIMIT = Number(process.env.LIMIT) // Maximum number of products can be added by a user at a time.
- 
-export { ADMINS, BOT_TOKEN, DB_URL, WORKER_URL, API_KEY, LIMIT }
+
+export { ADMINS, BOT_TOKEN, DB_URL, WORKER_URL, API_KEY, LIMIT, HTTPS_PROXY }
