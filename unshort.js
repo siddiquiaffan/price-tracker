9b9a282e3c9725371b0df44b17cc412def5b625b
siddiquiaffan <siddiquiaffan201@gmail.com>
2023-10-07 22:25:50 +0530

    Return URL as it is if it's already unshorted

--- unshort.js
+++ unshort.js
@@ -1,19 +1,48 @@
 import axios from 'axios';
+
+const merchants = [
+    'amazon',
+    'flipkart',
+    'snapdeal',
+    'ajio',
+    'myntra',
+    'jabong',
+    'bewakoof',
+    'limeroad',
+    'shein',
+]
+
+/**
+ * Get the long url from a short url
+ * @param {string} url 
+ * @returns 
+ */
 const unshort = async (url) => {
     const extractUrl = req => req?.request?.res?.responseUrl ||
         req?.request?._redirectable?._currentUrl ||
         req?.request?._currentUrl ||
         req?.request?._options?.href ||
         'https://' + req?.request?.host + req?.request?.path;
+
+
+    const host = new URL(url).hostname.split(".");
+    const merchant = host[0] === 'www' ? host[1] : host[0];
+
+    if (merchants.includes(merchant))
+        return url;
+
+    let longUrl = url;
+
     try {
         const req = await axios.get(url);
         const result = extractUrl(req);
-        var longUrl = result ? result : url;
+        longUrl = result ? result : url;
     } catch (err) {
         const result = extractUrl(err);
-        var longUrl = result ? result : url;
+        longUrl = result ? result : url;
     }
     return longUrl;
 }
+
 
 export default unshort;
